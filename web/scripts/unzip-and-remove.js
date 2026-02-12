7e8b8f43fd486cde2a883a2b52b25f05eb191779
xy <xiaoyi@tanyue.cc>
2026-02-12 10:38:12 +0800

    优化

--- web/scripts/unzip-and-remove.js
+++ web/scripts/unzip-and-remove.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const path = require('path');
 const AdmZip = require('adm-zip');
 
-// const TARGET_DIR = 'Z:\\陈靖\\1图片素材';
+const TARGET_DIR = 'Z:\\陈靖\\1图片素材';
 // const TARGET_DIR = 'Z:\\刘杰梅\\肖轶的图片素材';
 
 const getZipFiles = (dir, files = []) => {
