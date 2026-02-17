514b88b4509205c86459cbb72532954c62613550
xy <xiaoyi@tanyue.cc>
2026-02-17 21:18:29 +0800

    服务配置优化

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["192.168.10.116", "localhost"],
+  allowedDevOrigins: ["192.168.10.116", "localhost", "104.168.30.172", "10.61.75.69", "192.168.10.250"],
   experimental: {
     externalDir: true,
   },
